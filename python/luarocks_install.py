c28b2ce2ad29e66cdcbc74baf8ec160412e5c61d
Silletr <153998312+Silletr@users.noreply.github.com>
2025-07-10 13:32:34 +0300

    [CHANGED FILE/DIR: python/luarocks.py]
    Returned file to version with "luarocks install" (insted "luarocks upload")

--- python/luarocks_install.py
+++ python/luarocks_install.py
@@ -3,9 +3,8 @@ import sys
 
 print("<< LuaRocks installer started >>\n")
 
-# luarocks upload {lib_name}
 def install_lib(lib_name):
-    result = subprocess.run("luarocks", "upload", lib_name,
+    result = subprocess.run("luarocks", "install", lib_name,
             check=True,
             text=True,
             capture_output=True, 
