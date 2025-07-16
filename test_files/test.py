c4c101f10fa6f8322b56a04a196ebda695b13d0a
Silletr <hristicenkoigor764@gmail.com>
2025-07-16 21:34:45 +0000

    [ADDED FILE/DIR: test_files] Created test_files directory, moved all test. ... files to this directory
    	new file:   test_files/test.lua
    	new file:   test_files/test.py
    	new file:   test_files/test.rs

---
+++ test_files/test.py
@@ -0,0 +1,5 @@
+# File where u can write any library (even without using)
+# and when launch it project at your local-machine plugin be check
+# it library on pip list
+# Or just write on console command:
+# :SuggestImports {lib_names} even without import any lib
