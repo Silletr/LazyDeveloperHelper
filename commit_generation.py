901f1f53d769534b4526b67c0dd8f93c949f352b
Silletr <hristicenkoigor764@gmail.com>
2025-10-05 12:06:22 +0300

    [NEW FILE/DIR: commit_generation.py] ФÐCreated file for automated creating commits and reformated it with Ruff & Black formaters

---
+++ commit_generation.py
@@ -0,0 +1,59 @@
+"""
+EXMAPLE OF OUTPUT:
+    🗂️ Commit category:
+1. DELETED FILE/DIR
+2. CHANGED FILE/DIR
+3. BUGFIX IN FILE/DIR
+4. HOTFIX
+5. NEW FILE/DIR
+> 2
+
+❓ Files/dirs changed (comma separated):
+    python/cargo_install.py, .deepsource.toml
+
+📜 Short description:
+    Optimized script speed, added some files to ignores
+"""
+
+import subprocess
+
+
+# --- class for auto-commit generation
+class CommitGen:
+    def __init__(self) -> None:
+        self.msg = ""
+        self.categories = {
+            1: "DELETED FILE/DIR",
+            2: "CHANGED FILE/DIR",
+            3: "BUGFIX IN FILE/DIR",
+            4: "HOTFIX",
+            5: "NEW FILE/DIR",
+        }
+
+    def get_category(self) -> str:
+        print("\n🗂️ Commit category:")
+        for num, name in self.categories.items():
+            print(f"{num}. {name}")
+
+        while True:
+            try:
+                choice = int(input("> "))
+                if choice in self.categories:
+                    return self.categories[choice]
+                else:
+                    print("❌ Invalid choice, try again.")
+            except ValueError:
+                print("❌ Enter a number, not text.")
+
+    def run(self):
+        category = self.get_category()
+        file_dirs = input("\n❓ Files/dirs changed (comma separated):\n")
+        desc = input("\n📜 Description:\n")
+        msg = f"[{category}: {file_dirs}] {desc}"
+        print(f"\n✅ Commit message:\n{msg}")
+        subprocess.run(["git", "add", "."])
+        subprocess.run(["git", "commit", "-m", msg])
+
+
+if __name__ == "__main__":
+    CommitGen().run()
