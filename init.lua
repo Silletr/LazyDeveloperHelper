2665600648b25b76e9a616764f144f1f05f31638
Silletr <hristicenkoigor764@gmail.com>
2025-10-21 14:04:33 +0300

    [CHANGED FILE/DIR: init.lua, plugin/commands.lua, NEW FILE/DIR: commands/**] Big architecture update: commands are split across several files, so it's easier to add new ones

--- init.lua
+++ init.lua
@@ -1,7 +1,13 @@
 local M = {}
 
+local general = require("LazyDeveloperHelper.commands.general")
+local install = require("LazyDeveloperHelper.commands.install")
+local requirements = require("LazyDeveloperHelper.commands.install_requirements")
+
 function M.setup()
-  require("LazyDeveloperHelper.plugin.commands").commands()
+  general.register()
+  install.register()
+  requirements.register()
 end
 
 return M
