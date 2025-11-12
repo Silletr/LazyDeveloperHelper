ab01c7287322a2749518d60722db31d3490d5a32
Silletr <hristicenkoigor764@gmail.com>
2025-11-12 09:48:16 +0200

    [CODE REFORMAT: lua/LazyDeveloperHelper/python/requirements_installers/pip_req_installer.py] Reformated code, deleted un-used fragments, and etc

--- lua/LazyDeveloperHelper/python/requirements_installers/pip_req_installer.py
+++ lua/LazyDeveloperHelper/python/requirements_installers/pip_req_installer.py
@@ -1,33 +1,37 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 # --- IMPORTS ---
 import os
 import sys
 from subprocess import run, CalledProcessError
-from shutil import which
 
 # --- VARIABLES ---
 REQUIREMENTS_FILE = "requirements.txt"
 ALTERNATIVE_FILE = "requirements-dev.txt"
-pip_path = which("pip3")
 
 
 # --- LOGGING MESSAGE ---
 def log_message(message: str, level: str = "info") -> None:
-    prefixes = {"info": "📍", "success": "📦", "error": "❌"}
-    print(f"{prefixes.get(level, '📍')} {message}")
+    prefixes = {
+        "info": "\U0001f4cd",  # 📍
+        "success": "\U0001f4e6",  # 📦
+        "error": "\u274c",  # ❌
+    }
+    print(f"{prefixes.get(level, '\U0001f4cd')} {message}")
 
 
 # --- INSTALL REQUIREMENTS FROM FILE ---
 def install_requirements(search_path="."):
     for dirpath, _, filenames in os.walk(search_path):
         if REQUIREMENTS_FILE in filenames:
-            log_message("requirements.txt is found!", "success")
+            log_message(f"{REQUIREMENTS_FILE} is found!", "success")
             try:
                 run(
                     [
                         sys.executable,
                         "-m",
+                        "pip",
                         "install",
                         "-r",
                         os.path.join(dirpath, REQUIREMENTS_FILE),
@@ -36,19 +40,19 @@ def install_requirements(search_path="."):
                 )
                 return True
             except CalledProcessError as e:
-                log_message(f"Error installing requirements: {e}", "error")
+                log_message(f"Error installing {REQUIREMENTS_FILE}: {e}", "error")
                 return False
+
         elif ALTERNATIVE_FILE in filenames:
             log_message(
-                """requirements.txt not found,
-                using requirements-dev.txt""",
-                "info",
+                f"{REQUIREMENTS_FILE} not found, using {ALTERNATIVE_FILE}", "info"
             )
             try:
                 run(
                     [
                         sys.executable,
                         "-m",
+                        "pip",
                         "install",
                         "-r",
                         os.path.join(dirpath, ALTERNATIVE_FILE),
@@ -57,11 +61,11 @@ def install_requirements(search_path="."):
                 )
                 return True
             except CalledProcessError as e:
-                log_message(f"""Error installing requirements-dev.txt: {e}""", "error")
+                log_message(f"Error installing {ALTERNATIVE_FILE}: {e}", "error")
                 return False
+
     log_message(
-        f"""{REQUIREMENTS_FILE} or {ALTERNATIVE_FILE}
-        not found in {search_path}!""",
+        f"{REQUIREMENTS_FILE} or {ALTERNATIVE_FILE} not found in {search_path}!",
         "error",
     )
     return False
