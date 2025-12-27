db10de969c11bc79519386db57d0530c4b6b5b2f
Silletr <hristicenkoigor764@gmail.com>
2025-12-27 23:56:37 +0200

    [CODE REFACTOR: lua/LazyDeveloperHelper/python/java_installer/gradle_install.py | CHANGED FILE/DIR: lua/LazyDeveloperHelper/python/java_installer/build.gradle.kts] Fixed string lenght in python file, removed duplicates in .kts file

--- lua/LazyDeveloperHelper/python/java_installer/build.gradle.kts
+++ lua/LazyDeveloperHelper/python/java_installer/build.gradle.kts
@@ -9,9 +9,5 @@ repositories {
 
 
 dependencies {
-    implementation("retrofit:unknown:com.squareup.retrofit2:2.11.0")
     implementation("commons-lang3:unknown:commons-lang3:3.12.0")
-    implementation('retrofit:com.squareup.retrofit2:2.11.0')
-    implementation('retrofit:com.squareup.retrofit2:2.11.0')
-    implementation("org.apache.commons:commons-lang3:3.12.0")
-}
+    }
