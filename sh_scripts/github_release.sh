16fc0f61136c5a81cbb729f14f63804527103b9f
Silletr <hristicenkoigor764@gmail.com>
2026-01-03 17:18:22 +0200

    [TRYING TO FIX ARCHITECTURE EVEN NOW !!!]

--- sh_scripts/github_release.sh
+++ sh_scripts/github_release.sh
@@ -1,33 +1,27 @@
 github_release() {
-	echo "[*] Preparing clean release…"
+  echo "[*] Preparing clean release…"
 
-	rm -rf /tmp/ldh_release
-	mkdir -p /tmp/ldh_release/LazyDeveloperHelper
+  rm -rf /tmp/ldh_release
+  mkdir -p /tmp/ldh_release/LazyDeveloperHelper
 
-	echo "[*] Copying required files:"
+  echo "[*] Copying required files:"
 
-	cp -r lua/LazyDeveloperHelper/commands/ /tmp/ldh_release/LazyDeveloperHelper/
-	cp -r lua/LazyDeveloperHelper/init.lua /tmp/ldh_release/LazyDeveloperHelper/
-	cp -r lua/LazyDeveloperHelper/python /tmp/ldh_release/LazyDeveloperHelper/
-	cp -r plugin/ /tmp/ldh_release/LazyDeveloperHelper/
-	cp -r CHANGELOG.md /tmp/ldh_release/LazyDeveloperHelper/
+  cp -r lua/LazyDeveloperHelper/commands/ /tmp/ldh_release/LazyDeveloperHelper/
+  cp -r lua/LazyDeveloperHelper/init.lua /tmp/ldh_release/LazyDeveloperHelper/
+  cp -r lua/LazyDeveloperHelper/python /tmp/ldh_release/LazyDeveloperHelper/
+  echo "[*] Copying README.md, LICENSE files"
+  cp README.md /tmp/ldh_release/LazyDeveloperHelper/
+  cp commit_generation.py /tmp/ldh_release/LazyDeveloperHelper/
+  cp LICENSE /tmp/ldh_release/LazyDeveloperHelper/
 
-	echo "[*] Copying files for test workability"
-	cp -r test_files/test.* /tmp/ldh_release/LazyDeveloperHelper/
+  echo "[*] Zipping to /tmp/ldh_release:"
+  cd /tmp/ldh_release
+  zip -r LazyDeveloperHelper.zip LazyDeveloperHelper >/dev/null
 
-	echo "[*] Copying README.md, LICENSE files"
-	cp README.md /tmp/ldh_release/LazyDeveloperHelper/
-	cp commit_generation.py /tmp/ldh_release/LazyDeveloperHelper/
-	cp LICENSE /tmp/ldh_release/LazyDeveloperHelper/
+  echo "[*] Moving archive to LazyDeveloperHelper/dist:"
+  mkdir -p ./dist
+  mv LazyDeveloperHelper.zip ./dist/
 
-	echo "[*] Zipping to /tmp/ldh_release:"
-	cd /tmp/ldh_release
-	zip -r LazyDeveloperHelper.zip LazyDeveloperHelper >/dev/null
-
-	echo "[*] Moving archive to LazyDeveloperHelper/dist:"
-	mkdir -p ./dist
-	mv LazyDeveloperHelper.zip ./dist/
-
-	echo "[✓] Done. Clean .zip is in dist/, size:"
-	du -h dist/LazyDeveloperHelper.zip
+  echo "[✓] Done. Clean .zip is in dist/, size:"
+  du -h dist/LazyDeveloperHelper.zip
 }
--- sh_scripts/vim_org_release.sh
+++ sh_scripts/vim_org_release.sh
@@ -1,33 +1,25 @@
 vim_org_release() {
-	echo "[*] Preparing clean release…"
+  echo "[*] Preparing clean release…"
 
-	rm -f dist/LazyDeveloperHelper.zip
-	rm -rf /tmp/ldh_release
+  rm -f dist/LazyDeveloperHelper.zip
+  rm -rf /tmp/ldh_release
 
-	echo "[*] Copying folders"
-	mkdir -p /tmp/ldh_release/LazyDeveloperHelper
-	cp -r lua/LazyDeveloperHelper/commands/ /tmp/ldh_release/LazyDeveloperHelper/
-	cp -r lua/LazyDeveloperHelper/init.lua /tmp/ldh_release/LazyDeveloperHelper/
+  echo "[*] Copying folders"
+  mkdir -p /tmp/ldh_release/LazyDeveloperHelper
+  cp -r lua/LazyDeveloperHelper/commands/ /tmp/ldh_release/LazyDeveloperHelper/
+  cp -r lua/LazyDeveloperHelper/init.lua /tmp/ldh_release/LazyDeveloperHelper/
+  cp -r lua/LazyDeveloperHelper/python /tmp/ldh_release/LazyDeveloperHelper/
+  echo "[*] Copying License, commit-generator.py"
+  cp LICENSE commit_generation.py /tmp/ldh_release/LazyDeveloperHelper/
 
-	echo "[*] Copying files for test workability"
-	cp -r test_files/test.* /tmp/ldh_release/LazyDeveloperHelper/
+  echo "[*] Zipping..."
+  cd /tmp/ldh_release
+  zip -r LazyDeveloperHelper.zip LazyDeveloperHelper >/dev/null
 
-	cp -r lua/LazyDeveloperHelper/python/c_installers /tmp/ldh_release/LazyDeveloperHelper/
-	cp -r lua/LazyDeveloperHelper/python/requirements_installers /tmp/ldh_release/LazyDeveloperHelper/
-	find lua/LazyDeveloperHelper/python/java_installer -name 'gradle_install.py' -exec cp --parents {} /tmp/ldh_release/LazyDeveloperHelper/ \;
-	find lua/LazyDeveloperHelper/python/java_installer -name 'build.kotlin.kts' -exec cp --parents {} /tmp/ldh_release/LazyDeveloperHelper/ \;
+  echo "[*] Moving..."
+  mkdir -p ~/Projects/LazyDeveloperHelper/dist
+  mv LazyDeveloperHelper.zip ~/Projects/LazyDeveloperHelper/dist/
 
-	echo "[*] Copying License, commit-generator.py, changelog.md"
-	cp LICENSE commit_generation.py CHANGELOG.md /tmp/ldh_release/LazyDeveloperHelper/
-
-	echo "[*] Zipping..."
-	cd /tmp/ldh_release
-	zip -r LazyDeveloperHelper.zip LazyDeveloperHelper >/dev/null
-
-	echo "[*] Moving..."
-	mkdir -p ~/Projects/LazyDeveloperHelper/dist
-	mv LazyDeveloperHelper.zip ~/Projects/LazyDeveloperHelper/dist/
-
-	echo "[✓] Done. Clean .zip is in dist/, size:"
-	du -h ~/Projects/LazyDeveloperHelper/dist/LazyDeveloperHelper.zip
+  echo "[✓] Done. Clean .zip is in dist/, size:"
+  du -h ~/Projects/LazyDeveloperHelper/dist/LazyDeveloperHelper.zip
 }
